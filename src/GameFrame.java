333a9bb8225b8db072ca4aacd60c518f889de771
Nenad Zaric <nenadzaric.shone@gmail.com>
2021-10-18 02:30:08 +0200

    Last commit

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -23,14 +24,18 @@ public class GameFrame extends JFrame {
 	private JPanel contentPane;
 	private int dimension = 30;
 	private int cellSize = 20;
-	private int width = dimension*cellSize+25;
-	private int height = dimension*cellSize+110;
+	private int width = dimension*cellSize+15;
+	private int height = dimension*cellSize+105;
 	private Grid grid = new Grid(dimension, dimension);
 	private GamePanel g;
 	private boolean running = false;
-	/**
-	 * Launch the application.
-	 */
+	
+	//Definisanje boja
+	public Color gray = Color.decode("#676966");
+	public Color dark = Color.decode("#242220");
+	public Color light = Color.decode("#FAFAF8");
+
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -44,9 +49,7 @@ public class GameFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public GameFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 0, width, height);
@@ -62,6 +65,7 @@ public class GameFrame extends JFrame {
 		JMenu mnNew = new JMenu("New");
 		mnFil.add(mnNew);
 		
+		//Resetovanje grida
 		JMenuItem mntmEmpty = new JMenuItem("Empty");
 		mntmEmpty.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -75,6 +79,7 @@ public class GameFrame extends JFrame {
 		});
 		mnNew.add(mntmEmpty);
 		
+		//Generisanje nasumicnih celija
 		JMenuItem mntmRandom = new JMenuItem("Random");
 		mntmRandom.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -91,60 +96,230 @@ public class GameFrame extends JFrame {
 		JMenu mnOpen = new JMenu("Open");
 		mnFil.add(mnOpen);
 		
+		//Citanje matrice iz fajla
 		JMenuItem mntmFromFile = new JMenuItem("From file");
 		mntmFromFile.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setDialogTitle("Specify a file to open"); 
-
-		FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("csv files (*.csv)", "csv");
-
-		fileChooser.addChoosableFileFilter(csvFilter);
-		fileChooser.setFileFilter(csvFilter);
-
-		int userSelection = fileChooser.showOpenDialog(rootPane);
-		 
-		if (userSelection == JFileChooser.APPROVE_OPTION) {
-		    File fileToOpen = fileChooser.getSelectedFile();
-		    System.out.println("Save as file: " + fileToOpen.getAbsolutePath());
-		    
-		    try {
-		g.readFromFile(fileToOpen);
-		} catch (FileNotFoundException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-		}
-		}
-
-		}
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Specify a file to open"); 
+				//Filter za default ekstenziju
+				FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("csv files (*.csv)", "csv");
+		
+				fileChooser.addChoosableFileFilter(csvFilter);
+				fileChooser.setFileFilter(csvFilter);
+		
+				int userSelection = fileChooser.showOpenDialog(rootPane);
+				 
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+				    File fileToOpen = fileChooser.getSelectedFile();
+				    System.out.println("Save as file: " + fileToOpen.getAbsolutePath());
+				    
+				    try {
+				    	g.readFromFile(fileToOpen);
+				    } catch (FileNotFoundException e1) {
+				    	// TODO Auto-generated catch block
+				    	e1.printStackTrace();
+				    }
+				}
+			}
 		});
 		mnOpen.add(mntmFromFile);
 		
+		JMenu mnBuiltIn = new JMenu("Built In");
+		mnOpen.add(mnBuiltIn);
+		
+		JMenu mnStillLives = new JMenu("Still lives");
+		mnBuiltIn.add(mnStillLives);
+		
+		JMenuItem mntmBlock = new JMenuItem("Block");
+		mntmBlock.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Still lives/Block.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnStillLives.add(mntmBlock);
+		
+		JMenuItem mntmBeehive = new JMenuItem("Beehive");
+		mntmBeehive.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Still lives/Beehive.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnStillLives.add(mntmBeehive);
+		
+		JMenuItem mntmBoat = new JMenuItem("Boat");
+		mntmBoat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Still lives/Boat.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnStillLives.add(mntmBoat);
+		
+		JMenuItem mntmLoaf = new JMenuItem("Loaf");
+		mntmLoaf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Still lives/Loaf.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnStillLives.add(mntmLoaf);
+		
+		JMenu mnOscilators = new JMenu("Oscilators");
+		mnBuiltIn.add(mnOscilators);
+		
+		JMenuItem mntmBeacon = new JMenuItem("Beacon");
+		mntmBeacon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Oscilators/Beacons.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnOscilators.add(mntmBeacon);
+		
+		JMenuItem mntmBlinker = new JMenuItem("Blinker");
+		mntmBlinker.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Oscilators/Blinker.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnOscilators.add(mntmBlinker);
+		
+		JMenuItem mntmToad = new JMenuItem("Toad");
+		mntmToad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Oscilators/Toads.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnOscilators.add(mntmToad);
+		
+		JMenuItem mntmPulsar = new JMenuItem("Pulsar");
+		mntmPulsar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Oscilators/Pulsar.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnOscilators.add(mntmPulsar);
+		
+		JMenuItem mntmPentadecathlon = new JMenuItem("Pentadecathlon");
+		mntmPentadecathlon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Oscilators/Pentadecathlon.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnOscilators.add(mntmPentadecathlon);
+		
+		JMenu mnSpaceShips = new JMenu("Spaceships");
+		mnBuiltIn.add(mnSpaceShips);
+		
+		JMenuItem mntmGlider = new JMenuItem("Glider");
+		mntmGlider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Spaceships/Glider.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnSpaceShips.add(mntmGlider);
+		
+		JMenuItem mntmLWSS = new JMenuItem("Lightweight Spaceship");
+		mntmLWSS.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File fileToLoad = new File("src/Patterns/Spaceships/LightweightSpaceShip.csv");
+				try {
+					g.readFromFile(fileToLoad);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnSpaceShips.add(mntmLWSS);
+		
+		//Pisanje matrice u fajl
 		JMenuItem mntmSave = new JMenuItem("Save");
 		mntmSave.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setDialogTitle("Specify a file to save"); 
-
-		FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("csv files (*.csv)", "csv");
-
-		fileChooser.addChoosableFileFilter(csvFilter);
-		fileChooser.setFileFilter(csvFilter);
-		int userSelection = fileChooser.showSaveDialog(rootPane);
-		 
-		if (userSelection == JFileChooser.APPROVE_OPTION) {
-		    File fileToSave = fileChooser.getSelectedFile();
-		    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
-		    
-		try {
-		g.writeToFile(fileToSave);
-		} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-		}
-		}
-
-		}
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Specify a file to save"); 
+	
+				//Postavljanje default ekstenzije
+				FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("csv files (*.csv)", "csv");
+				fileChooser.addChoosableFileFilter(csvFilter);
+				fileChooser.setFileFilter(csvFilter);
+				int userSelection = fileChooser.showSaveDialog(rootPane);
+				 
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+				    File fileToSave = fileChooser.getSelectedFile();
+				    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+				    //Provera ekstenzije
+				    if (!fileToSave.getAbsolutePath().endsWith(".csv")) {
+				        fileToSave = new File(fileToSave.getAbsolutePath() + ".csv");
+				    }
+					try {
+						g.writeToFile(fileToSave);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+	
+			}
 		});
 		mnFil.add(mntmSave);
 		
@@ -154,22 +329,52 @@ public class GameFrame extends JFrame {
 				System.exit(EXIT_ON_CLOSE);
 			}
 		});
+		
+		//Eksportovanje stanja u sliku
+		JMenuItem mntmExport = new JMenuItem("Export to PNG");
+		mntmExport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Specify a file to save"); 
+				//Filter za default ekstenziju
+				FileNameExtensionFilter pngFilter = new FileNameExtensionFilter("png files (*.png)", "png");
+		
+				fileChooser.addChoosableFileFilter(pngFilter);
+				fileChooser.setFileFilter(pngFilter);
+				int userSelection = fileChooser.showSaveDialog(rootPane);
+				 
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+				    File fileToSave = fileChooser.getSelectedFile();
+				    //Provera ekstenzije
+				    if (!fileToSave.getAbsolutePath().endsWith(".png")) {
+				        fileToSave = new File(fileToSave.getAbsolutePath() + ".png");
+				    }
+					g.ExportPNG(fileToSave);
+				}
+
+			}
+		});
+		mnFil.add(mntmExport);
 		mnFil.add(mntmExit);
 		
 		g = new GamePanel(grid,cellSize);
 		getContentPane().add(g);
 		g.setLayout(null);
 		
+		//Dugme za jedan korak
 		JButton btnStep = new JButton("Step");
+		btnStep.setBackground(light);
 		btnStep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				g.step();
 			}
 		});
-		btnStep.setBounds(128, 608, 117, 25);
+		btnStep.setBounds(145, 608, 117, 25);
 		g.add(btnStep);
 		
+		//Dugme za pokretanje simulacije
 		JButton btnStart = new JButton("Start");
+		btnStart.setBackground(light);
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(!running) {
@@ -185,7 +390,7 @@ public class GameFrame extends JFrame {
 				}
 			}
 		});
-		btnStart.setBounds(312, 608, 117, 25);
+		btnStart.setBounds(331, 608, 117, 25);
 		g.add(btnStart);
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,15 +1,23 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -22,19 +30,25 @@ public class GamePanel extends JPanel implements ActionListener {
 	private int offset;
 	private Grid grid;
 	private Timer timer;
-	/**
-	 * Create the panel.
-	 */
+	
+	//Definisanje boja
+	public Color primary = Color.decode("#F2CB05");
+	public Color gray = Color.decode("#676966");
+	public Color dark = Color.decode("#242220");
+	public Color light = Color.decode("#FAFAF8");
+
+	
 	public GamePanel(Grid grid, int cellSize) {
 		super();
 		this.cellSize = cellSize;
 		this.offset = cellSize;
 		this.grid = grid;
-		columns = grid.getxDimension();
-		rows = grid.getyDimension();
+		this.columns = grid.getxDimension();
+		this.rows = grid.getyDimension();
+		
 		setBounds(offset/2,offset/2,columns*cellSize*2,rows*cellSize*2);
 		setVisible(true);
-		setBackground(Color.black);
+		setBackground(dark);
 		
 		addMouseListener(new MouseListener() {
 			@Override
@@ -48,32 +62,58 @@ public class GamePanel extends JPanel implements ActionListener {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				//Event Listener koji registruje klikove i azurira matricu celija
+				playSound();
+				registerClick(e);
 				
-				//Koordinate klika
-				int xCoordinate = e.getX()/cellSize;
-				int yCoordinate = e.getY()/cellSize;
-				
-				try {
-					System.out.println("X:" + xCoordinate + " Y:" + yCoordinate);
-					
-					//Menja stanje celije na koju je kliknuto
-					if(!grid.getCellGrid()[yCoordinate][xCoordinate].isAlive()) {
-						grid.getCellGrid()[yCoordinate][xCoordinate].setAlive(true);
-					}
-					else {
-						grid.getCellGrid()[yCoordinate][xCoordinate].setAlive(false);
-					}
-					revalidate();
-					repaint();
-				} catch (Exception e1) {
-				}
 			}
 		});
+	}
+
+	public void playSound() {
+		AudioInputStream audioInputStream;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(new File("src/pop.wav").getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (UnsupportedAudioFileException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (LineUnavailableException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	public void registerClick(MouseEvent e) {
+		//Koordinate klika
+		int xCoordinate = e.getX()/cellSize;
+		int yCoordinate = e.getY()/cellSize;
+		
+		try {
+			System.out.println("X:" + xCoordinate + " Y:" + yCoordinate);
+			
+			//Menja stanje celije na koju je kliknuto
+			if(!grid.getCellGrid()[yCoordinate][xCoordinate].isAlive()) {
+				grid.getCellGrid()[yCoordinate][xCoordinate].setAlive(true);
+			}
+			else {
+				grid.getCellGrid()[yCoordinate][xCoordinate].setAlive(false);
+			}
+			revalidate();
+			repaint();
+		} catch (Exception e1) {
+		}
 	}
 	
 	
 	
 	public void writeToFile(File gridFile) throws IOException {
+		//Metoda za upisivanje matrice u fajl
 		BufferedWriter br = new BufferedWriter(new FileWriter(gridFile));
 		int counter = 0;
 		for(int i=0; i<columns; i++) {
@@ -110,11 +150,14 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	
 	public void readFromFile(File gridFile) throws FileNotFoundException {
+		//Metoda za citanje matrice iz fajla
+		
 		Cell[][] newCellGrid = new Cell[columns][rows];
 		Scanner scan = new Scanner(gridFile);  
 		scan.useDelimiter(",");
 		int counter = 0;
 		int counterb = 0;
+		
 		for(int i=0; i<rows; i++) {
 			for(int j=0; j<columns; j++) {
 				
@@ -143,13 +186,28 @@ public class GamePanel extends JPanel implements ActionListener {
 		scan.close();
 		}
 	
+	public void ExportPNG(File temp) {
+		//Metoda koja eksportuje sadrzaj panela u sliku
+		BufferedImage image = new BufferedImage(getWidth(), getHeight()-50, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = image.createGraphics();
+		paint(g);
+		try{
+			ImageIO.write(image, "png", temp); 
+		} catch (IllegalArgumentException e) {
+			JOptionPane.showMessageDialog(null, "Vec postoji slika sa tim imenom");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void step() {
 		//Metoda koja azurira stanje celija u gridu nakon svakog koraka
-		System.out.println("Step taken");
+		playSound();
 		Cell[][] newCellGrid = new Cell[columns][rows];
 		for(int i=0;i<rows;i++) {
 			for(int j=0; j<columns; j++) {
-				
+				//Zanemaruje celije na ivici grida
 				if(i==0 || i== columns-1 || j==0 || j==rows-1) {
 					Cell c = new Cell(grid.getCellGrid()[i][j].isAlive());
 					newCellGrid[i][j] = c;
@@ -157,13 +215,16 @@ public class GamePanel extends JPanel implements ActionListener {
 				}
 				
 				int neighbourCount = grid.countNeighbours(i,j);
+				
+				//Ako je celija ziva i ima dve ili tri zive komsijske celije ona ostaje ziva
 				if(grid.getCellGrid()[i][j].isAlive() && (neighbourCount == 2 || neighbourCount ==3)) {
 					Cell c = new Cell(true);
 					newCellGrid[i][j] = c;
 				}
+				//Ako je celija mrtva i ima tacno tri zive komsijske celije ona postaje ziva
 				else if(!grid.getCellGrid()[i][j].isAlive() && neighbourCount==3) {
 					Cell c = new Cell(true);
-					newCellGrid[i][j] = c;				
+					newCellGrid[i][j] = c;
 				}
 				else {
 					Cell c = new Cell(false);
@@ -198,16 +259,16 @@ public class GamePanel extends JPanel implements ActionListener {
 					state = true;
 				}
 				if(!state) {
-					g.setColor(Color.black);
+					g.setColor(dark);
 				}
 				else if(state) {
-					g.setColor(Color.green);
+					g.setColor(primary);
 				}
 				g.fillRect(i*cellSize, j*cellSize, cellSize, cellSize);			
 			}
 		}
 		//Iscrtava linije koje cine "mrezu"
-		g.setColor(Color.lightGray);
+		g.setColor(light);
 		for(int i=0; i<rows;i++) {
 			g.drawLine(0, i*cellSize, rows*cellSize-5, i*cellSize);
 		}
@@ -218,7 +279,8 @@ public class GamePanel extends JPanel implements ActionListener {
 		for(int i=0; i<rows; i++) {
 			for(int j=0; j<columns; j++) {
 				if(i==0 || i== columns-1 || j==0 || j==rows-1) {
-					g.setColor(Color.darkGray);
+					g.setColor(gray
+							);
 					g.fillRect(i*cellSize, j*cellSize, cellSize, cellSize);			
 
 				}			
